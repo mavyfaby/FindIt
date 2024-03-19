70f5cf584e809bf806d1c0500fdcfe95cccc35f1
mavyfaby <maverickfabroa@gmail.com>
2024-03-19 21:59:15 +0800

    style(app): add styles and headers

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(() => {
+  $("#darkmode-switch").on("change", () => {
+    console.log(localStorage.getItem("dark"));
+    // Toggle class on dark body based on localstorage value of isdark
+    $("body").toggleClass("dark", localStorage.getItem("dark") === "0");
+    // Set localstorage value of isdark to 1 if body has class dark
+    localStorage.setItem("dark", $("body").hasClass("dark") ? "1" : "0");
+  });
+});
